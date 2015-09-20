6f40d3ea217057861d8d1cb9d299efd0d610af7f
Shivam Choudhary <sc3973@columbia.edu>
2015-09-19 20:20:38 -0400

    hmwk1:Adding support to populate the user with password

--- Common.py
+++ Common.py
@@ -5,3 +5,16 @@ def read_config(fname = "config.json"):
         configuration = json.load(conf)
     return configuration
 
+def load_password(fname):
+    user_passdict = {}
+    with open(fname) as userpassf:
+        userpassf  = userpassf.read().splitlines()
+        for val in userpassf:
+            vals = val.split(" ")
+            print vals[0]
+
+        return user_passdict
+
+
+
+load_password('user_pass.txt')
