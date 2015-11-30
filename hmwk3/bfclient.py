022ef866786927d372bd2ef4f5d05e39b8e99592
Shivam Choudhary <sc3973@columbia.edu>
2015-11-29 20:03:31 -0500

    Classical way for handling the inputs

--- hmwk3/bfclient.py
+++ hmwk3/bfclient.py
@@ -1,29 +1,33 @@
 import sys
+import argparse
 import os
 
-
 class bfClient(object):
+    """
+    Class to manage all the clients.
+    param:
+    """
 
-    def __init__(self):
+    def __init__(self,port,timeout,ipaddress1,port1,weight1,*args):
         self.ip = '127.0.0.1'
-
-
-
-
+        self.port = port
+        self.timeout = timeout
+        self.ipaddress1 = ipaddress1
+        self.port1 = port1
+    def converge(self):
+        pass
 
 
 def main():
-    try:
-        localport = int(sys.argv[1])
-        timeout = int(sys.argv[2])
-    except IndexError:
-        print "python bfclient.py <localport> <timeout> <ipaddress1> <port1>"
-        "<weight1> ... "
-        sys.exit(2)
-    except KeyboardInterrupt:
-        print "Control -C pressed!!"
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
-
+    parser = argparse.ArgumentParser(description='Bellman-Ford Distributed'
+            'Algorithm')
+    parser.add_argument("localport", type=int, help="local socket for binding!")
+    parser.add_argument("timeout", type=int, help="timeout for the client(in seconds)")
+    parser.add_argument("ipaddress1", type=str, help="IP Address of a Neighbour")
+    parser.add_argument("port1",type=int,help="Corresponding Port number of Neighbour")
+    parser.add_argument("weight1",type=float,help="A real number indicating cost of link")
+    parser.add_argument("optional",nargs='?',default=None,help="Other Arguments")
+    args = parser.parse_args()
+    print args
+if __name__=="__main__":
+    main()
