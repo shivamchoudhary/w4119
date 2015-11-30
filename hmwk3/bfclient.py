624384b115c4b1e19fc14c5e21e1b0c2cd266a31
Shivam Choudhary <sc3973@columbia.edu>
2015-11-30 17:27:47 -0500

    Adding docstring for init

--- hmwk3/bfclient.py
+++ hmwk3/bfclient.py
@@ -4,19 +4,33 @@ import os
 import Common
 import socket
 import select
+
+
 class bfClient(object):
     """
     Class to manage all the clients.
     """
 
-    def __init__(self,port,timeout,ipaddress1,port1,weight1,*args):
+    def __init__(self,localport,timeout,ipaddress1,port1,weight1,*args):
+        """
+        param:localport Local port on which the client is hosted.
+        param:timeout The timeout related with the client.
+        param:ipaddress1 IP Address of First Neighbour.
+        param:weight1 Weight of the link.
+        param:*args Arguments (3 tuples) for optional neighbour.
+        """
         self.ip = '127.0.0.1' #Each client is binded on localhost
-        self.port = port
+        self.localport = localport
         self.timeout = timeout
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.bind((self.ip, self.port))
+        self.sock.bind((self.ip, self.localport))
         neighbourTable = Common.Table()
-        neighbourTable.add_neighbour(ipaddress1, port1)
+        neighbourTable.add_neighbour((ipaddress1, port1),weight1)
+        if args is not None:
+            for arg in args:
+                print arg
+
+
         print neighbourTable.show_neighbour()
     def converge(self):
         pass
@@ -35,16 +49,18 @@ def main():
             "of Neighbour")
     parser.add_argument("weight1",type=float,help="A real number indicating"
             " cost of link")
-    parser.add_argument("optional",nargs='*',default=None,help="Other "
-            "Arguments")
+    parser.add_argument("--optional",nargs='*',default = None,help="Other Arguments")
     args = parser.parse_args()
-    if len(args.optional)%3 !=0:
-        raise ValueError ("Check Optional Arguments")
-        sys.exit(2)
-    else:
-        print args
-        client = bfClient(args.localport, args.timeout, args.ipaddress1, 
-                args.port1, args.weight1, args.optional)
+    print args
+    if args.optional is not None:
+        if len(args.optional)%3 !=0:
+            raise ValueError ("Check Optional Arguments")
+            sys.exit(2)
+        else:
+            args.optional = zip(*[args.optional[i::3] for i in range(3)])
+            print args.optional
+    client = bfClient(args.localport, args.timeout, args.ipaddress1, 
+                args.port1, args.weight1,args.optional)
         
 if __name__=="__main__":
     main()
